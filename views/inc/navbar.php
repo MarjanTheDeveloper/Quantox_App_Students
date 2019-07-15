11ee21a037ddaa79e1e2282dcacbacb374f62331
MarjanTheDeveloper <marjan_90_ks@yahoo.com>
2019-07-15 23:45:53 +0200

    v4

--- views/inc/navbar.php
+++ views/inc/navbar.php
@@ -1,6 +1,6 @@
 <div class="navbar navbar-expand-lg navbar-dark bg-primary">
 	<div class="container">
-		<a href="../" class="navbar-brand">QUANTOX School</a>
+		<a href="index.php" class="navbar-brand">QUANTOX School</a>
 		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
 		<span class="navbar-toggler-icon"></span>
 		</button>
